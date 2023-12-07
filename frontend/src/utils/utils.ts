9d6c333da8925842b215680ca0f04528c867f3b0
Maher, Michael <michael.maher1@ge.com>
2023-12-07 15:03:06 -0500

    SCRUM-1 #in progress #done #time 2d 4h Total work

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -52,7 +52,7 @@ export const integerToDateTime = (integerDate: number) => {
 };
 
 export const getTotalBox = (boxes: Box[]): Box => {
-  //this method will get Total Box.
+  //this method will get Total ss Box.
   let minX = Infinity;
   let minY = Infinity;
   let maxX = -Infinity;
